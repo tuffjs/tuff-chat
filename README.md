06f47b5d3353bfdc649fd646204e30849265a76e
Brian Haak <avesus8@gmail.com>
2017-07-26 13:42:41 -0700

    Checking if Heroku Postgres will work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tuff-chat
 A Comprehensive Example of How To Use TuffJS in Production
+
+Demo: https://tuff-chat.herokuapp.com/
+
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const port = process.env.PORT || 8080;
 
+// Format: postgres://user:password@server:5432/database
+const DATABASE_URL = process.env.DATABASE_URL;
+
+const Promise = require('bluebird');
+const pgp = require('pg-promise')({ promiseLib: Promise });
+const sql = pgp(DATABASE_URL);
+
 const server = require('http')
   .createServer(
     (request, response) => {
-      response.end('Hello from Heroku!');
+      sql.one(`SELECT 1`, req.body)
+        .then(one => {
+          response.end(`Hello from Heroku!<br/>SQL SELECT 1 returned ${ one }`);
+        });
     }
   )
   .listen(port,

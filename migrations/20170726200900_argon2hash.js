a3a2bef76cf217c26e459df965f66453eac52627
Brian Haak <avesus8@gmail.com>
2017-07-26 20:39:11 -0700

    ALTER COLUMN doesn't need NOT NULL, it's preserved.

--- migrations/20170726200900_argon2hash.js
+++ migrations/20170726200900_argon2hash.js
@@ -1,14 +1,14 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.raw(`ALTER TABLE "user" DROP COLUMN salt;`),
-    knex.raw(`ALTER TABLE "user" ALTER COLUMN hash TYPE TEXT NOT NULL;`),
+    knex.raw(`ALTER TABLE "user" ALTER COLUMN hash TYPE TEXT;`),
   ]);
 }
 
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.raw(`ALTER TABLE "user" ADD COLUMN salt BYTEA NOT NULL;`),
-    knex.raw(`ALTER TABLE "user" ALTER COLUMN hash TYPE BYTEA NOT NULL;`),
+    knex.raw(`ALTER TABLE "user" ALTER COLUMN hash TYPE BYTEA;`),
   ]) ;
 };
 

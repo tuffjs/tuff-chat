cf16575af582a7a1c385db5de9be9204de7fdc45
Brian Haak <avesus8@gmail.com>
2017-09-01 22:09:21 -0700

    Webpack now embedding bundle's hash, so hot reloader can use it

--- client/hot-update.js
+++ client/hot-update.js
@@ -1,17 +1,22 @@
 module.exports = {
   receive: function (message) {
     if (message.cltCodeHash) {
-      window.bundleHash = message.cltCodeHash;
+      // This is unreliable. In the message, it is last compiled bundle hash;
+      // Because we not packing the hash inside of our assets,
+      // in rare race conditions client code and server will have different hashes.
+
+      // window.JAVASCRIPT_BUNDLE_HASH = message.cltCodeHash;
+      // Instead, we'll reload new version from server.
       return true;
     } else if (message.debug) {
 
       if (message.debug.file) {
-        if (window.bundleHash !== message.debug.hash) {
+        if (window.JAVASCRIPT_BUNDLE_HASH !== message.debug.hash) {
           console.log('New file compiled', message.debug.hash,
-            window.bundleHash);
+            window.JAVASCRIPT_BUNDLE_HASH);
           var fileStr = window.atob(message.debug.file);
 
-          window.bundleHash = message.debug.hash;
+          window.JAVASCRIPT_BUNDLE_HASH = message.debug.hash;
           eval(fileStr);
         }
 

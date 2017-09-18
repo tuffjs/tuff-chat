16469a9b6c8fa0814bd5d8eeff751ec85062844a
Brian Haak <avesus8@gmail.com>
2017-09-18 15:10:13 -0700

    Logging of the file size when Webpack produces buggy result

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ assetsCompiler.onComplete((jsBundleFile, mapFile, hash, stats) => {
       `Browser-side assets reported that bundled successfully but found no hash. Skipping.
        Add window.JAVASCRIPT_BUNDLE_HASH = 'REPLACE_WEBPACK_HASH' at the most beginning of main entry`,
       hash);
-    console.log(jsBundleFile.size);
+    console.log(jsBundleFile.length);
     return;
   }
   jsBundleFile.write(hash, hashPos, 20);
